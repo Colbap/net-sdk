cca0941d2db47fbb98ad8566001185c0387f4e56
Pablo Cicchino <pcicchino@nuba.com.ar>
2014-04-07 11:37:41 -0300

    One AssemblyInfo for all the solution

---
+++ VersionInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("MercadoLibre")]
+[assembly: AssemblyProduct("MercadoLibre's .NET SDK")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
